3715fdcbee13340fb867f1bd49a95e66d29cc2b4
davefcodes <davefaibil@gmail.com>
2023-03-08 00:32:55 -0500

    changed files

--- assignments/Even_Or_Odd/src/com/davefcode/Even_Or_Odd.java
+++ assignments/Even_Or_Odd/src/com/davefcode/Even_Or_Odd.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Even_Or_Odd {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
-        System.out.println("Enter a number:");
+        System.out.println("Enter a number: ");
         int num = in.nextInt();
 
         if (num % 2 == 0){
