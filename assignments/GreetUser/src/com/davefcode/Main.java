06cac1ae3de942117fc27c5427c705839287e4ac
davefcodes <davefaibil@gmail.com>
2023-03-08 00:03:25 -0500

    assignment-02

---
+++ assignments/GreetUser/src/com/davefcode/Main.java
@@ -0,0 +1,18 @@
+package com.davefcode;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+
+        String name = input.next();
+
+        System.out.println("Hello " + name);
+
+
+    }
+
+
+}
