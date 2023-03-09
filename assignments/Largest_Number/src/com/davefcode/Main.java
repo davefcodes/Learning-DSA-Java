7aaf447ffcda528151962c7d28e4255cef2c5ee2
davefcodes <davefaibil@gmail.com>
2023-03-09 05:31:29 -0500

    assignment complete

---
+++ assignments/Largest_Number/src/com/davefcode/Main.java
@@ -0,0 +1,23 @@
+package com.davefcode;
+
+import java.util.Scanner;
+
+// Take 2 numbers as input and print the largest number.
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int num1 = input.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = input.nextInt();
+
+        if (num1 > num2){
+            System.out.println("The largest number is " + num1);
+        }
+        else {
+            System.out.println("The largest number is " + num2);
+        }
+    }
+}
